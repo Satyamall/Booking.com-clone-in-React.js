8e8efa085f18a02aae3171909e2d12707a954f1a
Satyamall <thesatyamall123@gmail.com>
2022-01-13 19:13:22 +0530

    Satya, React car Rentals Page and footer part

--- booking.com/src/App.js
+++ booking.com/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import CarRentals from './CarRentalsPage/CarRentals';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <CarRentals />
+      <Footer />
     </div>
   );
 }
